ff4d5da1553e01b3608cdb0d7b9041e3dc4a7e9a
Steven Choi <stychoiengr@gmail.com>
2022-12-17 01:16:59 -0800

    turned multi word rule off in eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-console': 'warn' // TODO: Make this an error for pushes to master
+    'no-console': 'warn', // TODO: Make this an error for pushes to master
+    'vue/multi-word-component-names': 'off', 
   },
   parserOptions: {
     parser: '@babel/eslint-parser'
