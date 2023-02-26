b601964db5271a03366ffb17141cf233e300fccb
Ponmo <ponmonw@gmail.com>
2023-02-25 19:17:11 -0800

    Fixed frontend testing error

--- frontend/tests/unit/components/SelectedEvents.spec.js
+++ frontend/tests/unit/components/SelectedEvents.spec.js
@@ -61,7 +61,7 @@ describe('SelectedEvents.vue', () => {
         it('the events are rendered with their names', () => {
             const courses = wrapper.findAll('.selected-course');
             expect(courses.length).toBe(2);
-            expect(courses.at(0).find('.event-name').text()).toBe('DUM 200')
+            expect(courses.at(0).find('.event-name').text()).toBe('DUM 200 (FULL)')
         });
 
         it('the courses are rendered with their titles', () => {
